b690e9c45733860d9f6fd608b1ecd4eefa0bc801
Karukan0814 <46077409+Karukan0814@users.noreply.github.com>
2024-02-10 12:12:04 -0800

    Task2: ファイル加工完了確認

--- client.py
+++ client.py
@@ -2,6 +2,7 @@ import socket
 import sys
 import os
 import json
+import time
 
 from lib import checkInputFile
 
@@ -10,96 +11,151 @@ JSON_SIZE_BYTES = 16
 PAYLOAD_SIZE_BYTES = 47
 HEADER_SIZE = JSON_SIZE_BYTES + PAYLOAD_SIZE_BYTES
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# サーバが待ち受けているポートにソケットを接続します
-server_address = "localhost"
-server_port = 9001
+def request_server():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-print("connecting to {}:{}".format(server_address, server_port))
+    # サーバが待ち受けているポートにソケットを接続します
+    server_address = "localhost"
+    server_port = 9001
 
-try:
-    # 接続後、サーバとクライアントが相互に読み書きができるようになります
-    sock.connect((server_address, server_port))
-except socket.error as err:
-    print(err)
-    sys.exit(1)
+    print("connecting to {}:{}".format(server_address, server_port))
 
-try:
-    file_path = input("Type in a file to upload: ")
-    # ファイルを送信する場合は、mp4ファイルで4GB以下に制限
-    checkFileResult = checkInputFile(file_path)
+    try:
+        # 接続後、サーバとクライアントが相互に読み書きができるようになります
+        sock.connect((server_address, server_port))
+    except socket.error as err:
+        print(err)
+        sys.exit(1)
 
-    if not checkFileResult:
-        raise Exception("File Error")
+    try:
+        file_path = input("Type in a file to upload: ")
+        # ファイルを送信する場合は、mp4ファイルで4GB以下に制限
+        checkFileResult = checkInputFile(file_path)
 
-    # 動画ファイルをアップロード
-    media_type = "mp4"  # 送信するメディアの種類
-    mode = input(
-        "何してほしいか。1:圧縮 2:解像度変更 3:アスペクト変更 4:音声変換 5:GIF作成 "
-    )
-    print("mode", mode)
+        if not checkFileResult:
+            raise Exception("File Error")
 
-    json_data = {"file_path": file_path, "media_type": media_type, "mode": mode}
+        # 動画ファイルをアップロード
+        media_type = "mp4"  # 送信するメディアの種類
+        mode = input(
+            "何してほしいか。1:圧縮 2:解像度変更 3:アスペクト変更 4:音声変換 5:GIF作成 "
+        )
+        print("mode", mode)
 
-    if mode == "2":
-        # 解像度変更の場合、解像度を入力して送信
-        resolution = input("解像度を入力してください (例: '640:360'): ")
-        json_data["resolution"] = resolution
-    elif mode == "3":
-        # アスペクト比変更の場合、アスペクト比を入力して送信
-        aspect_ratio = input("アスペクト比を入力してください (例: '16:9'): ")
-        json_data["aspect_ratio"] = aspect_ratio
-    elif mode == "5":
-        # GIF作成の場合、フォーマット、開始時間、終了時間を入力して送信
-        format = input("フォーマットを選択してください (gif または webm): ")
-        json_data["format"] = format
+        json_data = {"file_path": file_path, "media_type": media_type, "mode": mode}
 
-        start_time = input("開始時間を入力してください (例: '00:00:10'): ")
-        json_data["start_time"] = start_time
+        if mode == "2":
+            # 解像度変更の場合、解像度を入力して送信
+            resolution = input("解像度を入力してください (例: '640:360'): ")
+            json_data["resolution"] = resolution
+        elif mode == "3":
+            # アスペクト比変更の場合、アスペクト比を入力して送信
+            aspect_ratio = input("アスペクト比を入力してください (例: '16:9'): ")
+            json_data["aspect_ratio"] = aspect_ratio
+        elif mode == "5":
+            # GIF作成の場合、フォーマット、開始時間、終了時間を入力して送信
+            format = input("フォーマットを選択してください (gif または webm): ")
+            json_data["format"] = format
 
-        end_time = input("終了時間を入力してください (例: '00:00:20'): ")
-        json_data["end_time"] = end_time
-    elif mode == "1" or mode == "4":
-        pass
-    else:
-        print("無効なモードです")
-        raise Exception("Invalid mode was selected")
+            start_time = input("開始時間を入力してください (例: '00:00:10'): ")
+            json_data["start_time"] = start_time
 
-    # バイナリモードでファイルを読み込む
-    with open(file_path, "rb") as f:
-        # ファイル名をペイロードに
-        filename = os.path.basename(f.name)
-        json_data["file_name"] = filename
+            end_time = input("終了時間を入力してください (例: '00:00:20'): ")
+            json_data["end_time"] = end_time
+        elif mode == "1" or mode == "4":
+            pass
+        else:
+            print("無効なモードです")
+            raise Exception("Invalid mode was selected")
 
-        # JSON サイズ、メディアタイプ、ペイロードサイズを計算
-        json_payload = json.dumps(json_data)
-        json_payload_bytes = json_payload.encode("ISO-8859-1")
+        # バイナリモードでファイルを読み込む
+        with open(file_path, "rb") as f:
+            # ファイル名をペイロードに
+            filename = os.path.basename(f.name)
+            json_data["file_name"] = filename
 
-        json_size = len(json_payload_bytes)
-        media_type = json_data["media_type"]
-        payload_size = os.path.getsize(json_data["file_path"])
-        print("json_string", json_payload)
+            # JSON サイズ、メディアタイプ、ペイロードサイズを計算
+            json_payload = json.dumps(json_data)
+            json_payload_bytes = json_payload.encode("ISO-8859-1")
 
-        # ヘッダを作成
-        header = json_size.to_bytes(JSON_SIZE_BYTES, byteorder="big")
-        header += media_type.encode("utf-8")
-        header += payload_size.to_bytes(PAYLOAD_SIZE_BYTES, byteorder="big")
+            json_size = len(json_payload_bytes)
+            media_type = json_data["media_type"]
+            payload_size = os.path.getsize(json_data["file_path"])
+            print("json_string", json_payload)
 
-        # ヘッダとペイロードを送信
-        sock.sendall(header)
+            # ヘッダを作成
+            header = json_size.to_bytes(JSON_SIZE_BYTES, byteorder="big")
+            header += media_type.encode("utf-8")
+            header += payload_size.to_bytes(PAYLOAD_SIZE_BYTES, byteorder="big")
 
-        # JSONデータを送信
-        print("send_jsondata", json_payload_bytes)
-        sock.send(json_payload_bytes)
+            # ヘッダとペイロードを送信
+            sock.sendall(header)
 
-        # ファイルを送信
-        data = f.read(1400)
-        while data:
-            print("Sending...")
-            sock.send(data)
+            # JSONデータを送信
+            print("send_jsondata", json_payload_bytes)
+            sock.send(json_payload_bytes)
+
+            # ファイルを送信
             data = f.read(1400)
+            while data:
+                print("Sending...")
+                sock.send(data)
+                data = f.read(1400)
 
-finally:
-    print("closing socket")
-    sock.close()
+            # サーバーからファイルIDを受け取る
+            file_id = sock.recv(1024).decode()  # サーバからのレスポンスを受け取る
+            print("file_id", file_id)
+
+            # ファイル送信後、一分ごとにファイルの状態をサーバに聞く
+            # 聞いた結果が”完了”だったら処理終了
+            # 処理中であればまた一分後にサーバーに状態を聞く
+
+            ask_server(file_id)
+
+    finally:
+        print("closing socket")
+        sock.close()
+
+
+def ask_server(file_id: str):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # サーバが待ち受けているポートにソケットを接続します
+    server_address = "localhost"
+    server_port = 9002
+
+    print("connecting to {}:{}".format(server_address, server_port))
+
+    try:
+        # 接続後、サーバとクライアントが相互に読み書きができるようになります
+        sock.connect((server_address, server_port))
+    except socket.error as err:
+        print(err)
+        sys.exit(1)
+    try:
+        while True:
+            # ここでサーバに処理状態を問い合わせるリクエストを送る
+            sock.sendall("status".encode())  # 状態確認用のキーワードを送信
+            status = sock.recv(1024).decode()  # サーバからのレスポンスを受け取る
+            print("status", status)
+            if status == "completed":
+                print("Processing completed.")
+                break
+            else:
+                print("Processing... Waiting another minute.")
+                time.sleep(60)  # 60秒待つ
+
+    except Exception as e:
+        print(f"Error: {e}")
+
+    finally:
+        print("Closing socket2.")
+        sock.close()
+
+
+def main():
+    request_server()
+
+
+main()
--- server.py
+++ server.py
@@ -1,6 +1,9 @@
 import socket
 import os
 import json
+import threading
+import time
+
 
 from pathlib import Path
 from lib import (
@@ -12,134 +15,215 @@ from lib import (
 )
 
 
-# このステージでは、ユーザーが動画ファイルをサーバにアップロードし、そのファイルに対して動画処理ができるサービスを開発します。処理が完了した動画ファイルはサーバからユーザーに返されます。サーバとクライアントは、TCP とカスタムアプリケーションレベルプロトコルを使って、さまざまな種類のファイルを互いに送信できるようにします。以下に、このサービスで提供される動画処理機能の一覧があります。
-
-# 動画を圧縮する: ユーザーは動画ファイルをサーバにアップロードし、圧縮されたバージョンをダウンロードできます。サーバが自動的に最適な圧縮を選択します。
-# 動画の解像度を変更する: ユーザーが動画をアップロードして希望する解像度を選択すると、その解像度に変更された動画がダウンロードできます。
-# 動画のアスペクト比を変更する: ユーザーが動画をアップロードして希望するアスペクト比を選ぶと、そのアスペクト比に変更された動画がダウンロードできます。
-# 動画を音声に変換する: 動画ファイルをアップロードすると、その動画から音声だけを抽出した MP3 バージョンがダウンロードできます。
-# 指定した時間範囲で GIF や WEBM を作成: ユーザーが動画をアップロードし、時間範囲を指定すると、その部分を切り取って GIF または WEBM フォーマットに変換します。
-# すべての動画処理は FFMPEG を使って行います。
-
-# まず、必要なモジュールをインポートし、ソケットオブジェクトを作成して、アドレスファミリ（AF_INET）とソケットタイプ（SOCK_STREAM）を指定します。サーバのアドレスは、任意のIPアドレスからの接続を受け入れるアドレスである0.0.0.0に設定し、サーバのポートは9001に設定されています。
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_address = "0.0.0.0"
-server_port = 9001
-
-# MMP プロトコル関連の定数
-JSON_SIZE_BYTES = 16
-MEDIA_TYPE_BYTES = 1
-PAYLOAD_SIZE_BYTES = 47
-HEADER_SIZE = JSON_SIZE_BYTES + MEDIA_TYPE_BYTES + PAYLOAD_SIZE_BYTES
+# 受け付けたファイルの情報を保存するクラス
+class File:
+    def __init__(self, file_id: str, input_path: str, output_path: str, state=0):
+        self.file_id = file_id  # 被らないようにタイムスタンプをidとして利
+        self.state = state  # 0 はリクエスト、1 は処理中、2 は完了
+        self.input_path = input_path
+        self.output_path = output_path
 
 
-# 次に、現在の作業ディレクトリに「temp」という名前のフォルダが存在するかどうかをチェックします。存在しない場合は、os.makedirs() 関数を使用してフォルダを作成します。このフォルダは、クライアントから受信したファイルを格納するために使用されます。
-input_path = "input"
-if not os.path.exists(input_path):
-    os.makedirs(input_path)
-output_path = "output"
-if not os.path.exists(output_path):
-    os.makedirs(output_path)
+# 受け付けたファイルのリスト
+file_map: {str: File} = {}
 
-print("Starting up on {} port {}".format(server_address, server_port))
 
-# 次に、サーバは bind()関数を使用して、ソケットをサーバのアドレスとポートに紐付けします。その後、listen()関数を呼び出すことで、サーバは着信接続の待ち受けを開始します。サーバは一度に最大1つの接続を受け入れることができます。
-sock.bind((server_address, server_port))
+def accept_file():
 
-sock.listen(1)
+    # まず、必要なモジュールをインポートし、ソケットオブジェクトを作成して、アドレスファミリ（AF_INET）とソケットタイプ（SOCK_STREAM）を指定します。サーバのアドレスは、任意のIPアドレスからの接続を受け入れるアドレスである0.0.0.0に設定し、サーバのポートは9001に設定されています。
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_address = "0.0.0.0"
+    server_port = 9001
 
-while True:
-    # その後、サーバは無限ループに入り、クライアントからの着信接続を継続的に受け付けます。このコードでは、accept()関数を使用して、着信接続を受け入れ、クライアントのアドレスを取得します。
-    connection, client_address = sock.accept()
-    try:
-        print("connection from", client_address)
-        # 次に、クライアントから受信したデータのヘッダを読み取り、変数headerに格納します。ヘッダには、ファイル名の長さ、JSON データの長さ、クライアントから受信するデータの長さに関する情報が含まれています。
-        header = connection.recv(HEADER_SIZE)
+    # MMP プロトコル関連の定数
+    JSON_SIZE_BYTES = 16
+    MEDIA_TYPE_BYTES = 1
+    PAYLOAD_SIZE_BYTES = 47
+    HEADER_SIZE = JSON_SIZE_BYTES + MEDIA_TYPE_BYTES + PAYLOAD_SIZE_BYTES
 
-        # ヘッダから各種情報を抜き出す
-        json_size = int.from_bytes(header[:JSON_SIZE_BYTES], byteorder="big")
-        media_type = header[
-            JSON_SIZE_BYTES : JSON_SIZE_BYTES + MEDIA_TYPE_BYTES
-        ].decode()
-        payload_size = int.from_bytes(header[-PAYLOAD_SIZE_BYTES:], byteorder="big")
-        print("json_size", json_size)
+    # 次に、現在の作業ディレクトリに「temp」という名前のフォルダが存在するかどうかをチェックします。存在しない場合は、os.makedirs() 関数を使用してフォルダを作成します。このフォルダは、クライアントから受信したファイルを格納するために使用されます。
+    input_path = "input"
+    if not os.path.exists(input_path):
+        os.makedirs(input_path)
+    output_path = "output"
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
 
-        # # ファイル名を受信
-        # filename = connection.recv(1024).decode()
-        # print("Filename: {}".format(filename))
+    print("Starting up on {} port {}".format(server_address, server_port))
 
-        # JSONデータを受信
+    # 次に、サーバは bind()関数を使用して、ソケットをサーバのアドレスとポートに紐付けします。その後、listen()関数を呼び出すことで、サーバは着信接続の待ち受けを開始します。サーバは一度に最大1つの接続を受け入れることができます。
+    sock.bind((server_address, server_port))
 
-        data = b""
-        while len(data) < json_size + 2:
-            packet = connection.recv(json_size + 2 - len(data))
-            if not packet:
-                break
-            data += packet
+    sock.listen(1)
 
-        # data = connection.recv(json_size + 2)
-        print("data", data)
-        # バイト文字列をデコードしてUnicode文字列に変換
-        # test_string = data.decode("utf-8")
-        unicode_string = data.decode("ISO-8859-1")
-        print("unicode_string", unicode_string)
+    while True:
+        # その後、サーバは無限ループに入り、クライアントからの着信接続を継続的に受け付けます。このコードでは、accept()関数を使用して、着信接続を受け入れ、クライアントのアドレスを取得します。
+        connection, client_address = sock.accept()
+        try:
+            print("connection from", client_address)
+            # 次に、クライアントから受信したデータのヘッダを読み取り、変数headerに格納します。ヘッダには、ファイル名の長さ、JSON データの長さ、クライアントから受信するデータの長さに関する情報が含まれています。
+            header = connection.recv(HEADER_SIZE)
 
-        # 'u\xd0' を削除して、残りの部分を取得
-        cleaned_string = unicode_string.replace("u\xd0", "")
-        print("cleaned_string", cleaned_string)
+            # ヘッダから各種情報を抜き出す
+            json_size = int.from_bytes(header[:JSON_SIZE_BYTES], byteorder="big")
+            media_type = header[
+                JSON_SIZE_BYTES : JSON_SIZE_BYTES + MEDIA_TYPE_BYTES
+            ].decode()
+            payload_size = int.from_bytes(header[-PAYLOAD_SIZE_BYTES:], byteorder="big")
+            print("json_size", json_size)
 
-        json_payload = json.loads(cleaned_string)
+            # JSONデータを受信
 
-        print("json_payload", json_payload)
-
-        mode = json_payload["mode"]
-        file_name = json_payload["file_name"]
-        print("file_name", file_name)
-        print("mode", mode)
-
-        stream_rate = 1400
-
-        # ファイルを書き込む準備
-        with open(os.path.join(input_path, file_name), "wb") as file:
-            # ファイルの内容を受信し、ファイルに書き込む
-            received_bytes = 0
-            while received_bytes < payload_size:
-                data = connection.recv(stream_rate)
-                # print("data", data)
-                if not data:
+            data = b""
+            while len(data) < json_size + 2:
+                packet = connection.recv(json_size + 2 - len(data))
+                if not packet:
                     break
-                file.write(data)
-                received_bytes += len(data)
-                print("received_bytes", received_bytes)
+                data += packet
 
-        print("Finished downloading the file from client.")
+            # data = connection.recv(json_size + 2)
+            # バイト文字列をデコードしてUnicode文字列に変換
+            unicode_string = data.decode("ISO-8859-1")
 
-        # 以下、モードごとにinputfileを加工する
-        output_file_path = ""
-        if mode == "1":
-            output_file_path = compress_video(file_name)
-        elif mode == "2":
-            resolution = json_payload["resolution"]
-            output_file_path = change_resolution(file_name, resolution)
-        elif mode == "3":
-            aspect_ratio = json_payload["aspect_ratio"]
-            output_file_path = change_aspect_ratio(file_name, aspect_ratio)
-        elif mode == "4":
-            output_file_path = convert_to_audio(file_name)
-        elif mode == "5":
-            format = json_payload["format"]
+            # 'u\xd0' を削除して、残りの部分を取得
+            cleaned_string = unicode_string.replace("u\xd0", "")
 
-            start_time = json_payload["start_time"]
-            end_time = json_payload["end_time"]
-            output_file_path = create_gif_or_webm(
-                file_name, start_time, end_time, format
-            )
-        print("output_file_path", output_file_path)
-        # sock.sendall(output_file_path.encode("utf-8"))
+            json_payload = json.loads(cleaned_string)
 
-    except Exception as e:
-        print("Error: " + str(e))
+            print("json_payload", json_payload)
 
-    finally:
-        print("Closing current connection")
-        connection.close()
+            mode = json_payload["mode"]
+            file_name = json_payload["file_name"]
+            print("file_name", file_name)
+            print("mode", mode)
+
+            stream_rate = 1400
+
+            # ファイルを書き込む準備
+            with open(os.path.join(input_path, file_name), "wb") as file:
+                # ファイルの内容を受信し、ファイルに書き込む
+                received_bytes = 0
+                while received_bytes < payload_size:
+                    data = connection.recv(stream_rate)
+                    # print("data", data)
+                    if not data:
+                        break
+                    file.write(data)
+                    received_bytes += len(data)
+                    print("received_bytes", received_bytes)
+
+            # # ファイル名＋タイムスタンプ→intで一意なfile_idを作成する
+            # # 現在のタイムスタンプを取得
+            # timestamp = str(time.time())
+            # # ファイル名とタイムスタンプを結合
+            # file_id = file_name + timestamp
+
+            # # ファイルリストに入れる
+            # file_info = File(file_id, input_path + file_name, "")
+            # file_map[file_id] = file_info
+
+            # print("filemap", file_map)
+
+            print("Finished downloading the file from client.")
+
+            # 以下、モードごとにinputfileを加工する
+            output_file_path = ""
+            if mode == "1":
+                output_file_path = compress_video(file_name)
+            elif mode == "2":
+                resolution = json_payload["resolution"]
+                output_file_path = change_resolution(file_name, resolution)
+            elif mode == "3":
+                aspect_ratio = json_payload["aspect_ratio"]
+                output_file_path = change_aspect_ratio(file_name, aspect_ratio)
+            elif mode == "4":
+                output_file_path = convert_to_audio(file_name)
+            elif mode == "5":
+                format = json_payload["format"]
+
+                start_time = json_payload["start_time"]
+                end_time = json_payload["end_time"]
+                output_file_path = create_gif_or_webm(
+                    file_name, start_time, end_time, format
+                )
+            print("output_file_path", output_file_path)
+            # 加工処理が終わったらファイルリストの該当ファイルの状態を変更
+            # file_map[file_id].state = 2
+            # print("filemap", file_map)
+
+        except Exception as e:
+            print("Error: " + str(e))
+
+        finally:
+            print("Closing current connection")
+            connection.close()
+
+
+def respond_file_state():
+    # 現在処理中のファイル一覧の処理結果を確認
+
+    # まず、必要なモジュールをインポートし、ソケットオブジェクトを作成して、アドレスファミリ（AF_INET）とソケットタイプ（SOCK_STREAM）を指定します。サーバのアドレスは、任意のIPアドレスからの接続を受け入れるアドレスである0.0.0.0に設定し、サーバのポートは9001に設定されています。
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_address = "0.0.0.0"
+    server_port = 9002
+
+    print("Starting up on {} port {}".format(server_address, server_port))
+
+    # 次に、サーバは bind()関数を使用して、ソケットをサーバのアドレスとポートに紐付けします。その後、listen()関数を呼び出すことで、サーバは着信接続の待ち受けを開始します。サーバは一度に最大1つの接続を受け入れることができます。
+    sock.bind((server_address, server_port))
+
+    sock.listen(1)
+
+    while True:
+        # その後、サーバは無限ループに入り、クライアントからの着信接続を継続的に受け付けます。このコードでは、accept()関数を使用して、着信接続を受け入れ、クライアントのアドレスを取得します。
+        connection, client_address = sock.accept()
+        try:
+            print("connection from", client_address)
+            # クライアントが確認したいファイルのIDを取り出す
+            file_id = connection.recv(1024)
+
+            # 処理中のファイルリストから該当のファイルを取り出す
+            if int(file_id.decode("utf-8")) in file_map:
+
+                file_Info = file_map[int(file_id.decode("utf-8"))]
+                state = file_Info.state
+                print("file_id", file_id)
+                print("state", state)
+
+                if state == "1" or state == "0":
+                    sock.sendall("処理中".encode("utf-8"))
+                elif state == "2":
+                    sock.sendall("完了".encode("utf-8"))
+                    output_path = file_Info.output_path
+                    sock.sendall(output_path.encode("utf-8"))
+
+                    # 処理中ファイルリストから削除する
+                    removed_file = file_map.pop(file_id, None)
+                    print(removed_file.file_id + "を処理が正常終了したため削除しました")
+                else:
+                    sock.sendall("異常終了".encode("utf-8"))
+
+            else:
+                sock.sendall("No file".encode("utf-8"))
+
+        except Exception as e:
+            print("Error: " + str(e))
+
+        finally:
+            print("Closing current connection")
+            connection.close()
+
+
+def main():
+    # 新規のファイル加工を受け付けるスレッド
+    accept_file_thread = threading.Thread(target=accept_file)
+
+    # 加工が終わったファイルの情報をクライアント側に投げるスレッド
+    respond_thread = threading.Thread(target=respond_file_state)
+
+    # スレッドを開始
+    accept_file_thread.start()
+    respond_thread.start()
+
+
+main()
